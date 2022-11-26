fd9824f6a87d5e0ddc023c1591c40a6a7b75554a
Felipe Tavares Costa <112184097+ZtFelipinZt@users.noreply.github.com>
2022-11-26 15:15:04 -0300

    Create tabuada.java

---
+++ Clássicos/tabuada.java
@@ -0,0 +1,17 @@
+public abstract class tabuada {
+
+	public static void main(String[] args) {
+		int i=1,n=1,tabuada;
+	while(n<=10) {	
+		i=1;
+		System.out.println(" ");
+		do {
+			tabuada = n*i;
+			System.out.println(n+"X"+i+"="+tabuada);
+			i++;
+		}while (i<=10);
+		n = n+1;
+		}
+	}
+
+}
